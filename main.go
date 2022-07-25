2f7f5a835b7aee5965bf90d5f5c6321e2cfe2638
zatrasz75 <109724241+zatrasz75@users.noreply.github.com>
2022-07-25 10:29:59 +0300

    Add files via upload
    calc

--- main.go
+++ main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
-	"fmt"
+	fmt "fmt"
+	"helloapp/calc"
 	"helloapp/maths"
 )
 
 func main() {
+	//от прошлого задания
 	fmt.Println(maths.E)
 	fmt.Println(maths.B)
+
+	//------------
+	x := calc.ReadIntager1()
+	s := calc.ReadStr()
+	y := calc.ReadIntager2()
+
+	switch s {
+	case "+":
+		fmt.Println(x + y)
+	case "-":
+		fmt.Println(x - y)
+	case "*":
+		fmt.Println(x * y)
+	case "/":
+		if y != 0 {
+			fmt.Println(x / y)
+		} else {
+			fmt.Printf("Нельзя делить число на %g", y)
+		}
+
+	}
 }
